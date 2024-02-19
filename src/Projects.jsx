201ab9f48149e0db6a01f0015acfdc478a212171
Aijamal Jusupova <vivaaijagmail.com>
2024-02-19 05:44:05 +0000

    added Projects.jsx to src

---
+++ src/Projects.jsx
@@ -0,0 +1,16 @@
+// Projects.jsx
+import React from 'react';
+import projects from './projects.json'; // Import the project data
+
+function Projects() {
+  console.log(projects); // Log the project data to the console
+
+  // Render the projects
+  return (
+    <div>
+      {/* Render your projects here */}
+    </div>
+  );
+}
+
+export default Projects;
